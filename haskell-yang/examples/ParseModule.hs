a4fd68f8915313d99ebafc9ef142d344c20942b8
Ladislav Lhotka <lhotka@nic.cz>
2015-05-22 15:10:11 +0200

    Extend the example.

--- haskell-yang/examples/ParseModule.hs
+++ haskell-yang/examples/ParseModule.hs
@@ -1,12 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Data.Attoparsec.ByteString.Lazy
+import Data.Attoparsec.ByteString
 import System.Environment
-import Data.Yang.Module.Parser
-import qualified Data.ByteString.Lazy as B
+import Data.Yang.Module
+import qualified Data.ByteString as B
 
 main :: IO ()
-main = do args <- getArgs
-          let fn = head args
-          bs <- B.readFile fn
-          parseTest yModule bs
+main = do
+    args <- getArgs
+    let fn = head args
+    bs <- B.readFile fn
+    let
+      m = case parseOnly yModule bs of
+        Left _ -> error "broken module"
+        Right s -> s
+    print $ findAll (BuiltIn "container") m
